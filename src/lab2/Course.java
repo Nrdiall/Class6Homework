6847a23c890953df5a0fb3e87b521dffc8a5c92c
Naython <Naython@Naython.attlocal.net>
2017-09-17 16:59:47 -0500

    Lab 2 - Created the interface super class.

---
+++ src/lab2/Course.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author Naython
+ */
+public interface Course {
+    
+    public abstract void setCourseNumber();
+    public abstract void setCredits();
+    public abstract String getCourseNumber();
+    public abstract String getCredits();
+    
+}
